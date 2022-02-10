89eac80ad2ff2bd43a6e8f52b4934f37d54dc1e9
Adam <asfmadx@gmail.com>
2022-02-10 11:01:04 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Wordle // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your green letters (Use underscores for blanks)");
+            var green = Console.ReadLine();
+            Console.WriteLine("Enter your yellow letters (Use underscores for blanks)");
+            var yellow = Console.ReadLine();
+            Console.WriteLine("Enter your grey letters");
+            var grey = Console.ReadLine();
+
+            Wordle.Guess(green, yellow, grey);
+        }
+    }
+}
