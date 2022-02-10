010f066efcec265f6eb26624f1affab578f87a32
Adam <asfmadx@gmail.com>
2022-02-10 11:23:55 -0500

    Fixes

--- Program.cs
+++ Program.cs
@@ -1,20 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-namespace Wordle // Note: actual namespace depends on the project name.
+namespace Wordle
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your green letters (Use underscores for blanks)");
-            var green = Console.ReadLine();
-            Console.WriteLine("Enter your yellow letters (Use underscores for blanks)");
-            var yellow = Console.ReadLine();
-            Console.WriteLine("Enter your grey letters");
-            var grey = Console.ReadLine();
+            string green = string.Empty;
+            string yellow = string.Empty;
+            string grey = string.Empty;
 
-            Wordle.Guess(green, yellow, grey);
+            while (green.Length != 5)
+            {
+                Console.WriteLine("Enter your green letters (Use underscores for blanks)");
+                green = Console.ReadLine() ?? string.Empty;
+            }
+
+            while (yellow.Length != 5)
+            {
+                Console.WriteLine("Enter your yellow letters (Use underscores for blanks)");
+                yellow = Console.ReadLine() ?? string.Empty;
+            }
+
+            Console.WriteLine("Enter your grey letters");
+            grey = Console.ReadLine() ?? String.Empty;
+
+            var guesses = Wordle.Guess(green, yellow, grey);
+
+            foreach (var guess in guesses)
+            {
+                Console.WriteLine(guess);
+            }
         }
     }
 }
